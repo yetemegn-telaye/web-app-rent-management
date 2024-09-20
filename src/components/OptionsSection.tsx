e6814596f4d8c4534eace9df42ce119244b0e54f
yetemegn-telaye <lemma19yet@gmail.com>
2024-09-20 13:46:08 +0300

    Create landlord layout and optionssection component

--- src/components/OptionsSection.tsx
+++ src/components/OptionsSection.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import Dropdown from './Dropdown';
 
+interface OptionsSectionProps {
+  buttonOptions: { label: string; primary?: boolean }[];
+  selectedOption: string; // Pass the selected option from parent
+  onOptionSelected: (label: string) => void;
+}
 
-const OptionsSection: React.FC = () => {
+const OptionsSection: React.FC<OptionsSectionProps> = ({ buttonOptions, selectedOption, onOptionSelected }) => {
   return (
     <div className="flex space-x-0 items-center justify-start mb-7">
-        <button className="bg-primary-dark  text-white py-2 px-4 flex items-center justify-between focus:outline-none">
-            Set Open House Date
+      {buttonOptions.map((option, index) => (
+        <button
+          key={index}
+          onClick={() => onOptionSelected(option.label)}
+          className={`py-2 px-4 flex items-center justify-between focus:outline-none ${
+            selectedOption === option.label ? 'bg-primary-dark text-white' : 'bg-white text-secondary-dark'
+          }`}
+        >
+          {option.label}
         </button>
-        <button className="bg-white text-secondary-dark py-2 px-4 flex items-center justify-between focus:outline-none">
-            View Manager
-        </button>
-        <button className="bg-white text-secondary-dark py-2 px-4 flex items-center justify-between focus:outline-none">
-            Document
-        </button>
+      ))}
     </div>
   );
 };
--- src/layout/LandlordLayout.tsx
+++ src/layout/LandlordLayout.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import Sidebar from "../components/SideBar";
 import Header from "../components/Header";
+import TenantSidebar from "../components/TenantSideBar";
 
 const LandlordLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="maxn-h-screen bg-primary">
+        <div className="max-h-vh bg-primary">
         <Header />
         <div className="flex text-sm"> 
             <Sidebar />
+            {/* <TenantSidebar/> */}
             <main className="flex-1 p-6">
                 {children}
             </main>
