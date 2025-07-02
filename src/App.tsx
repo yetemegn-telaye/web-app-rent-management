a84524b63477c07441633c7cc66d4cb3d367dd71
yetemegn-telaye <lemma19yet@gmail.com>
2025-07-02 15:41:39 +0300

    Add authcontext to make auth state persistent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './features/auth/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './features/auth/Login';
+import Signup from './features/auth/signUp';
 import Property from './features/property';
 import Listings from './features/listings';
 import ListingDetail from './features/listings/ListingDetail';
@@ -17,52 +17,43 @@ import Dashboard from './features/dashboard';
 import OnboardingScreen from './features/onbaordingScreeen';
 import PrivateRoute from './routes/PrivateRoute';
 import UnauthorizedPage from './features/UnauthorizedPage';
+import { AuthProvider } from './contexts/authContext';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Signup />} />
 
-        {/* <Route path="/getting-started" element={<OnboardingScreen />} />
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/property" element={<Property />} />
-        <Route path="/add-listing" element={<AddListing/>}/>
-        <Route path="/all-payments" element={<AllPayment/>} />
-        <Route path="/add-tenant" element={<AddTenant/>} />
-        <Route path="/all-tenants" element={<AllTenant/>}/>
-        <Route path="/settings" element={<Settings/>}/>
-        <Route path="/all-listing" element={<Listings/>} />
-        <Route path="/listing-detail/:id" element={<ListingDetail/>} />
-        <Route path="/notifications" element={<Notifications />}/>
-        <Route path="/my-rents" element={<MyRent/>}/> */}
+          <Route element={<PrivateRoute allowedRoles={['manager']} />} >
+            <Route path="/getting-started" element={<OnboardingScreen />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/property" element={<Property />} />
+            <Route path="/add-listing" element={<AddListing />} />
+            <Route path="/all-payments" element={<AllPayment />} />
+            <Route path="/add-tenant" element={<AddTenant />} />
+            <Route path="/all-tenants" element={<AllTenant />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
 
+          <Route element={<PrivateRoute allowedRoles={['manager', 'tenant']} />} >
+            <Route path="/all-listing" element={<Listings />} />
+            <Route path="/listing-detail/:id" element={<ListingDetail />} />
+            <Route path="/notifications" element={<Notifications />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={['building_manager']} />} >
-        <Route path="/getting-started" element={<OnboardingScreen />} />
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/property" element={<Property />} />
-        <Route path="/add-listing" element={<AddListing/>}/>
-        <Route path="/all-payments" element={<AllPayment/>} />
-        <Route path="/add-tenant" element={<AddTenant/>} />
-        <Route path="/all-tenants" element={<AllTenant/>}/>
-        <Route path="/settings" element={<Settings/>}/>
-        </Route>
+          <Route element={<PrivateRoute allowedRoles={['tenant']} />} >
+            <Route path="/my-rents" element={<MyRent />} />
+          </Route>
 
-        <Route element={<PrivateRoute allowedRoles={['building_manager','tenant']} />} >
-        <Route path="/all-listing" element={<Listings/>} />
-        <Route path="/listing-detail/:id" element={<ListingDetail/>} />
-        <Route path="/notifications" element={<Notifications />}/>
-        </Route>
-
-        <Route element={<PrivateRoute allowedRoles={['tenant']} />} >
-        <Route path="/my-rents" element={<MyRent/>}/>
-        </Route>
-       
-        <Route path="/unauthorized" element={<UnauthorizedPage />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/unauthorized" element={<UnauthorizedPage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-import type { RootState } from '../redux/store'; 
+import { auth } from '../firebase';
 
 const baseApi = createApi({
-    reducerPath: 'baseApi', 
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://rent.jasmineaddis.com',
-        prepareHeaders: (headers, { getState }) => {
-            // const token = (getState() as RootState).auth.token;
-            const token = localStorage.getItem('token');  
-            if (token) {
-                headers.set('Authorization', `Bearer ${token}`);
-            }
-            return headers;
-        }
-    }),
-    endpoints: (builder) => ({}),  
+  reducerPath: 'baseApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://rent.jasmineaddis.com',
+    prepareHeaders: async (headers) => {
+      const user = auth.currentUser;
+
+      if (user) {
+        const token = await user.getIdToken(); // Firebase JWT
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: () => ({}), // define your endpoints later
 });
 
 export default baseApi;
